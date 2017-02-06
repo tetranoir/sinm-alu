ecd84d40371c0f3f52589bae28ca45c5cb0f69ce
Huajie Wu <wuhuajie.flyfire2002@gmail.com>
2017-02-05 22:08:42 -0800

    reg_file implementation and testbench

--- Lab2/Lab2.v
+++ Lab2/Lab2.v
@@ -15,64 +15,49 @@
 
 // PROGRAM		"Quartus Prime"
 // VERSION		"Version 16.1.0 Build 196 10/24/2016 SJ Lite Edition"
-// CREATED		"Sun Feb 05 15:57:13 2017"
+// CREATED		"Sun Feb 05 17:44:02 2017"
 
 module Lab2(
-	SC_IN,
 	clk,
 	write_en,
 	data_in,
-	INPUTA,
-	INPUTB,
-	OP,
-	raddrA,
-	raddrB,
+	instr,
+	oprnd,
 	waddr,
-	SC_OUT,
 	data_outA,
+	data_outAddrBase,
 	data_outB,
-	OUT
+	dr_code
 );
 
 
-input wire	SC_IN;
 input wire	clk;
 input wire	write_en;
 input wire	[7:0] data_in;
-input wire	[7:0] INPUTA;
-input wire	[7:0] INPUTB;
-input wire	[3:0] OP;
-input wire	[3:0] raddrA;
-input wire	[3:0] raddrB;
+input wire	[4:0] instr;
+input wire	[3:0] oprnd;
 input wire	[3:0] waddr;
-output wire	SC_OUT;
 output wire	[7:0] data_outA;
+output wire	[7:0] data_outAddrBase;
 output wire	[7:0] data_outB;
-output wire	[7:0] OUT;
+output wire	[3:0] dr_code;
 
 
 
 
 
 
-ALU	b2v_inst(
-	.SC_IN(SC_IN),
-	.INPUTA(INPUTA),
-	.INPUTB(INPUTB),
-	.OP(OP),
-	.SC_OUT(SC_OUT),
-	.OUT(OUT));
-
-
-reg_file	b2v_inst6(
+reg_file	b2v_inst(
 	.clk(clk),
 	.write_en(write_en),
 	.data_in(data_in),
-	.raddrA(raddrA),
-	.raddrB(raddrB),
+	.instr(instr),
+	.oprnd(oprnd),
 	.waddr(waddr),
 	.data_outA(data_outA),
-	.data_outB(data_outB));
+	.data_outAddrBase(data_outAddrBase),
+	.data_outB(data_outB),
+	.dr_code(dr_code));
 
 
 endmodule
