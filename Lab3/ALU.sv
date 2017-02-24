fd0cc0e691fff634c66429f378f26149f2ecb306
JJ <jjtran.mail@gmail.com>
2017-02-23 16:00:34 -0800

    alu with enables

--- Lab3/ALU.sv
+++ Lab3/ALU.sv
@@ -16,8 +16,10 @@ module ALU (
     output logic TO_WRITE_MEM,
     output logic TO_READ_MEM, // bit signal for mem and data sel mux
 	
-	output logic [3:0]REG_NUM_OUT
-	
+	output logic [3:0]REG_NUM_OUT,
+	output logic write_reg,
+	output logic branch,
+	output logic write_flag
 	
 );
 
@@ -30,30 +32,54 @@ always_comb begin
     TO_WRITE_MEM = 0;
     TO_READ_MEM = 0;
 	FLAG_OUT = C_IN;
+	write_reg = 0;
+	branch = 0;
+	write_flag = 0;
 	
 
 	case(OP)
-		opSetdr: REG_NUM_OUT = INPUT_B[3:0];
-		opAdd: {FLAG_OUT, OUT} = INPUT_A + INPUT_B; //any overflow get stored in flagout
-		opAddc:  {FLAG_OUT, OUT} = INPUT_A + INPUT_B + C_IN;
-		opSub: OUT = INPUT_A - INPUT_B;
+		opSetdr: begin OUT = INPUT_B; write_reg = 1; end
+		opAdd: begin 
+			{FLAG_OUT, OUT} = INPUT_A + INPUT_B; //any overflow get stored in flagout
+			write_reg = 1;
+			write_flag = 1;
+			end
+		opAddc:  begin
+			{FLAG_OUT, OUT} = INPUT_A + INPUT_B + C_IN;
+			write_reg = 1;
+			write_flag = 1;
+			end
+		opSub: begin 
+			OUT = INPUT_A - INPUT_B;
+			write_reg = 1;
+			end
 		
-		opIncr: {FLAG_OUT, OUT} = INPUT_B + 8'h01;
-		opDecr: OUT = INPUT_B - 8'h01;
+		opIncr: begin
+			{FLAG_OUT, OUT} = INPUT_B + 8'h01;
+			write_reg = 1;
+			write_flag = 1;
+			end
+		opDecr: begin 
+			OUT = INPUT_B - 8'h01;
+			write_reg = 1;
+			end
 		
 		opLoadImm: begin
             OUT_ADDR = (INPUT_C << 4) + INPUT_B[3:0];
             TO_READ_MEM = 1;
+				write_reg = 1;
         end
 		opLoadReg: begin
-            OUT = INPUT_B;
+            OUT_ADDR = INPUT_B;
             TO_READ_MEM = 1;
+				write_reg = 1;
         end
 		
 		opStoreImm: begin
             OUT = INPUT_A;
             OUT_ADDR = (INPUT_C << 4) + INPUT_B[3:0];
             TO_WRITE_MEM = 1;
+				
         end
 		opStoreReg: begin
             OUT = INPUT_A;
@@ -61,82 +87,99 @@ always_comb begin
             TO_WRITE_MEM = 1;
         end
 		
-		opMovImm: OUT = INPUT_A[3:0];
-		opMovReg: OUT = INPUT_A;
+		opMovImm: begin OUT = INPUT_B[3:0]; write_reg = 1; end
+		opMovReg: begin OUT = INPUT_B; write_reg = 1; end
 		
-		opClr: {FLAG_OUT, OUT} = 0;
+		opClr: begin {FLAG_OUT, OUT} = 0; write_reg = 1; write_flag =1; end
 		
-		opShl: {FLAG_OUT, OUT} = INPUT_A << INPUT_B[3:0];
-		opShr: OUT = INPUT_A >> INPUT_B[3:0];
-		opSar: OUT = $signed(INPUT_A) >>> INPUT_B[3:0];
+		opShl: begin OUT = INPUT_A << INPUT_B[3:0]; write_reg = 1; end
+		opShr: begin OUT = INPUT_A >> INPUT_B[3:0]; write_reg = 1; end
+		opSar: begin OUT = $signed(INPUT_A) >>> INPUT_B[3:0]; write_reg = 1; end
 			   
 		
-		//REDO ASDFKJASDF
+		
 		opRcr: begin 
-					FLAG_OUT = INPUT_A[0];
-					OUT = ({C_IN, INPUT_A[7:1]});
+					FLAG_OUT = INPUT_B[0];
+					OUT = ({C_IN, INPUT_B[7:1]});
+					write_reg = 1;
+					write_flag = 1;
 				 end
 				 
 				 
 		opRcl: begin 
-					FLAG_OUT = INPUT_A[7];
-					OUT = ({INPUT_A[6:0], C_IN});
+					FLAG_OUT = INPUT_B[7];
+					OUT = ({INPUT_B[6:0], C_IN});
+					write_reg = 1;
+					write_flag = 1;
 				 end
 		
-		opAnd: OUT = INPUT_A & INPUT_B;
-		opXor: OUT = INPUT_A ^ INPUT_B;
+		opAnd: begin OUT = INPUT_A & INPUT_B; write_reg = 1; end
+		opXor: begin OUT = INPUT_A ^ INPUT_B; write_reg = 1; end
 		
 		opCmpImm:
 			if(INPUT_A == INPUT_B) begin
 				FLAG_OUT = 1;
+				write_flag = 1;
 			end else begin
 				FLAG_OUT = 0;
+				write_flag = 1;
 			end
 		opCmpReg:
 			if(INPUT_A == INPUT_B) begin
 				FLAG_OUT = 1;
+				write_flag = 1;
 			end else begin
 				FLAG_OUT = 0;
+				write_flag = 1;
 			end
 		
-		opJmp: OUT = (INPUT_C << 4) + INPUT_B[3:0];
+		opJmp: begin OUT = (INPUT_C << 4) + INPUT_B[3:0]; branch = 1; end
 		opJz:
 			if(INPUT_A == 0) begin
 				OUT = (INPUT_C << 4) + INPUT_B[3:0];
+				branch = 1;
 			end else begin 
 				OUT = 0;
 			end
 		opJfnz:
 			if(C_IN != 0) begin
 				OUT = (INPUT_C << 4) + INPUT_B[3:0];
+				branch = 1;
 			end else begin 
 				OUT = 0;
 			end
 		opJnz:
 			if(INPUT_A != 0) begin
 				OUT = (INPUT_C << 4) + INPUT_B[3:0];
+				branch = 1;
 			end else begin 
 				OUT = 0;
 			end
 		
 		opClfb: if(((INPUT_A ^ INPUT_B)& 8'h0F) == 0) begin
 						FLAG_OUT = 0;
+						write_flag = 1;
 					end else begin 
 						FLAG_OUT = 1; 
+						write_flag = 1;
 					end
 		opMax:
 			if(INPUT_A >= INPUT_B) begin
 				OUT = INPUT_A;
+				write_reg = 1;
 			end else begin 
 				OUT = INPUT_B; 
+				write_reg = 1;
 			end
 		
 	
 		opCkfr:
 			if((INPUT_A | INPUT_B) == 0) begin
 				FLAG_OUT = 0;
+				write_flag = 1;
 			end else begin
 				FLAG_OUT = 1;
+				write_flag = 1;
 			end
 			
 		
